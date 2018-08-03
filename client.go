03d6c1c7842c24d650e2dbd034bbc3d2dc1bb220
tomocy <tomocy.dev@gmail.com>
2018-08-04 06:47:57 +0900

    add method for client to read and write message

--- client.go
+++ client.go
@@ -7,3 +7,23 @@ type client struct {
 	send   chan []byte
 	room   *room
 }
+
+func (c *client) read() {
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.room.forward <- msg
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
