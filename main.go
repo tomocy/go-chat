25d1f6d7c6568a5f9bd18f1c8556c1c3998679c4
tomocy <tomocy.dev@gmail.com>
2018-08-04 21:36:44 +0900

    make room run background and make server listen and serve in main thred

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{fileName: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("could not listen and serve: %s", err)
 	}
