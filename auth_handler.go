8e598fa2b9bc35f8da3ed889593b905f37553e00
tomocy <tomocy.dev@gmail.com>
2018-08-06 07:49:15 +0900

    make auth handler implement http.Handler

--- auth_handler.go
+++ auth_handler.go
@@ -5,3 +5,21 @@ import "net/http"
 type authHandler struct {
 	next http.Handler
 }
+
+func MustAuth(h http.Handler) http.Handler {
+	return &authHandler{next: h}
+}
+
+func (a *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err != nil {
+		if err == http.ErrNoCookie {
+			w.Header().Set("Location", "/login")
+			w.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		}
+
+		panic(err)
+	}
+
+	a.next.ServeHTTP(w, r)
+}
