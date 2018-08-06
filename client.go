57ef6218aaf1c438f79969786558bf9fd36d5fcd
tomocy <tomocy.dev@gmail.com>
2018-08-06 14:17:48 +0900

    show avatar image on message

--- client.go
+++ client.go
@@ -21,6 +21,9 @@ func (c *client) read() {
 		}
 
 		msg.Name = c.user["name"].(string)
+		if avatarURL, ok := c.user["avatar_url"]; ok {
+			msg.AvatarURL = avatarURL.(string)
+		}
 		msg.When = time.Now()
 		c.room.forward <- msg
 	}
