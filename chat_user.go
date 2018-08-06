5af776db845c3a6ad6ca1ac68e846c040c274b79
tomocy <tomocy.dev@gmail.com>
2018-08-06 18:13:46 +0900

    add type and make it implement ChatUser

--- chat_user.go
+++ chat_user.go
@@ -1,6 +1,17 @@
 package main
 
+import gomniauthcommon "github.com/stretchr/gomniauth/common"
+
 type ChatUser interface {
 	UniqueID() string
 	AvatarURL() string
+}
+
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (c chatUser) UniqueID() string {
+	return c.uniqueID
 }
