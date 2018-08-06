cf4b02ac463881f45e097f0e5b3e94857aa74331
tomocy <tomocy.dev@gmail.com>
2018-08-07 00:52:20 +0900

    remove former avatar file when uploading one

--- uploader_handler.go
+++ uploader_handler.go
@@ -4,33 +4,60 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 func uploaderHandler(w http.ResponseWriter, r *http.Request) {
+	if err := setAvatarFile(r); err != nil {
+		log.Fatalf("could not set avatar file: %s\n", err)
+		return
+	}
+	w.Header().Set("Location", "/chat")
+	w.WriteHeader(http.StatusOK)
+}
+
+func setAvatarFile(r *http.Request) error {
 	userID := r.FormValue("userID")
+	if err := deleteAvatarFile(userID); err != nil {
+		return err
+	}
+
 	file, header, err := r.FormFile("avatarFile")
 	if err != nil {
-		log.Fatalf("could not get file and the header: %s\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 	defer file.Close()
 
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
-		log.Fatalf("could not read all of the file: %s\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	fileName := filepath.Join("avatars", userID+filepath.Ext(header.Filename))
 	if err := ioutil.WriteFile(fileName, data, 0777); err != nil {
-		log.Fatalf("could not write the data to file: %s\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return err
 	}
 
-	w.Header().Set("Location", "/chat")
-	w.WriteHeader(http.StatusOK)
+	return nil
+}
+
+func deleteAvatarFile(userID string) error {
+	dirName := "avatars"
+	files, err := ioutil.ReadDir(dirName)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		if match, _ := filepath.Match(userID+"*", file.Name()); match {
+			fileName := filepath.Join(dirName, file.Name())
+			os.Remove(fileName)
+		}
+	}
+
+	return nil
 }
