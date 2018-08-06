4f4001caa171b191329bf4718db3360006d8de97
tomocy <tomocy.dev@gmail.com>
2018-08-06 23:57:29 +0900

    add method for getting auth request

--- login_handler.go
+++ login_handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -13,35 +14,41 @@ import (
 	"github.com/stretchr/gomniauth"
 )
 
+type authRequest struct {
+	action   string
+	provider string
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	segs := strings.Split(r.URL.Path, "/")
-	action := segs[2]
-	provider := segs[3]
-	switch action {
+	authReq, err := getAuthRequest(r.URL.Path)
+	if err != nil {
+		log.Fatalf("could not get auth request: %s\n", err)
+	}
+	switch authReq.action {
 	case "login":
-		provider, err := gomniauth.Provider(provider)
+		provider, err := gomniauth.Provider(authReq.provider)
 		if err != nil {
-			log.Fatalln("could not get proivder %s: %s", provider, err)
+			log.Fatalln("could not get proivder %s: %s", authReq.provider, err)
 		}
 		loginURL, err := provider.GetBeginAuthURL(nil, nil)
 		if err != nil {
-			log.Fatalln("could not get login url of %s: %s", provider, err)
+			log.Fatalln("could not get login url of %s: %s", authReq.provider, err)
 		}
 
 		w.Header().Set("Location", loginURL)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	case "callback":
-		provider, err := gomniauth.Provider(provider)
+		provider, err := gomniauth.Provider(authReq.provider)
 		if err != nil {
-			log.Fatalln("could not get provider %s: %s", provider, err)
+			log.Fatalln("could not get provider %s: %s", authReq.provider, err)
 		}
 		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
 		if err != nil {
-			log.Fatalln("could not get user credentials from %s: %s", provider, err)
+			log.Fatalln("could not get user credentials from %s: %s", authReq.provider, err)
 		}
 		user, err := provider.GetUser(creds)
 		if err != nil {
-			log.Fatalln("could not get user from %s: %s", provider, err)
+			log.Fatalln("could not get user from %s: %s", authReq.provider, err)
 		}
 		chatUser := &chatUser{User: user}
 		m := md5.New()
@@ -66,6 +73,18 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "not supported action: %s", action)
+		fmt.Fprintf(w, "not supported action: %s", authReq.action)
 	}
+}
+
+func getAuthRequest(path string) (authRequest, error) {
+	segs := strings.Split(path, "/")
+	if len(segs) < 4 {
+		return authRequest{}, errors.New("the length of segs is not over 4")
+	}
+
+	return authRequest{
+		action:   segs[2],
+		provider: segs[3],
+	}, nil
 }
