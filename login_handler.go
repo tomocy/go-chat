87c521c8ca172cdb647381bb815df4c005d68cf8
tomocy <tomocy.dev@gmail.com>
2018-08-06 14:01:16 +0900

    save avatar_url in cookie

--- login_handler.go
+++ login_handler.go
@@ -42,7 +42,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("could not get user from %s: %s", provider, err)
 		}
 		authCookie := objx.New(map[string]interface{}{
-			"name": user.Name(),
+			"name":       user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
